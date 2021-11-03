6609f9e28d7636bcd09444a09b4f4d202c2b6d6e
Simon Farre <simon.farre.cx@gmail.com>
2021-11-03 17:57:35 +0100

    Commit before adding submodule to main

--- modules/activateDebuggerExtension.js
+++ modules/activateDebuggerExtension.js
@@ -1,11 +1,8 @@
 "use strict";
 
 const vscode = require("vscode");
-const {RRSession} = require("./debugSession");
-
-const rrdbg_unimplemented = (commandName) => {
-  vscode.window.showErrorMessage(`rrdbg.${commandName} not yet implemented`);
-};
+const { RRSession } = require("./debugSession");
+const { getVSCodeCommands } = require("./commandsRegistry");
 
 /**
  *
@@ -13,33 +10,38 @@ const rrdbg_unimplemented = (commandName) => {
  * @param {vscode.DebugAdapterDescriptorFactory} [descriptorFactory]
  */
 function activateExtension(context, descriptorFactory) {
-  let rr_start = vscode.commands.registerCommand("rrdbg.rr-start", () => rrdbg_unimplemented("rr-start"));
-  let rr_stop = vscode.commands.registerCommand("rrdbg.rr-stop", () => rrdbg_unimplemented("rr-stop"));
-  let getExecutable = vscode.commands.registerCommand("rrdbg.get-binary", () => rrdbg_unimplemented("get-binary"));
-  context.subscriptions.push(rr_start);
-  context.subscriptions.push(rr_stop);
-  context.subscriptions.push(getExecutable);
-
-  context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider("rrdbg", {
-    /**
-     * @param {vscode.WorkspaceFolder | undefined} folder
-     * @returns { vscode.ProviderResult<vscode.DebugConfiguration[]> }
-    */
-    provideDebugConfigurations(folder) {
-      return [
-        {
-          "name": "Launch",
-          "request": "launch",
-          "type": "rrdbg",
-          "program": "${config:rrdbg.bin}"
-        }
-      ]
-    }
-  }, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
-  if(!descriptorFactory) {
+  context.subscriptions.push(...getVSCodeCommands());
+  context.subscriptions.push(
+    vscode.debug.registerDebugConfigurationProvider(
+      "rrdbg",
+      {
+        /**
+         * @param {vscode.WorkspaceFolder | undefined} folder
+         * @returns { vscode.ProviderResult<vscode.DebugConfiguration[]> }
+         */
+        provideDebugConfigurations(folder) {
+          return [
+            {
+              name: "Launch",
+              request: "launch",
+              type: "rrdbg",
+              program: "${config:rrdbg.bin}",
+            },
+          ];
+        },
+      },
+      vscode.DebugConfigurationProviderTriggerKind.Dynamic
+    )
+  );
+  if (!descriptorFactory) {
     descriptorFactory = new RRDebugAdapterFactory();
   }
-  context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory("rrdbg", descriptorFactory));
+  context.subscriptions.push(
+    vscode.debug.registerDebugAdapterDescriptorFactory(
+      "rrdbg",
+      descriptorFactory
+    )
+  );
 }
 
 class RRDebugAdapterFactory {
@@ -49,11 +51,12 @@ class RRDebugAdapterFactory {
    * @returns ProviderResult<vscode.DebugAdapterDescriptor>
    */
   createDebugAdapterDescriptor(session) {
-    return new vscode.DebugAdapterInlineImplementation(new RRSession("rrdbg.log"));
+    return new vscode.DebugAdapterInlineImplementation(
+      new RRSession("rrdbg.log")
+    );
   }
 }
 
-
 module.exports = {
-  activateExtension
-}
+  activateExtension,
+};
