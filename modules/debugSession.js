cab2692179301b466b676701eefe51fc8453a801
Simon Farre <simon.farre.cx@gmail.com>
2021-11-03 22:56:26 +0100

    Move from desktop to laptop

--- modules/debugSession.js
+++ modules/debugSession.js
@@ -2,6 +2,7 @@
 
 const vscodeDebugAdapter = require("vscode-debugadapter");
 const vscode = require("vscode");
+const { DebugProtocol } = require("vscode-debugprotocol");
 const { GDBInterface } = require("./debuggerInterface");
 
 class RRSession extends vscodeDebugAdapter.LoggingDebugSession {
@@ -22,6 +23,86 @@ class RRSession extends vscodeDebugAdapter.LoggingDebugSession {
         new vscodeDebugAdapter.StoppedEvent("entry", this.#threadId)
       );
     });
+    this.#gdbInterface.on("stopOnBreakpoint", () => {
+      this.sendEvent(
+        new vscodeDebugAdapter.StoppedEvent("breakpoint", this.#threadId)
+      );
+    });
+  }
+  /**
+   * As per Mock debug adapter:
+   * The 'initialize' request is the first request called by the frontend
+   * to interrogate the features the debug adapter provides.
+   */
+  initializeRequest(response, args) {
+    if (args.supportsProgressReporting) this._reportProgress = true;
+    if (args.supportsInvalidatedEvent) this.useInvalidetedEvent = true;
+    // build and return the capabilities of this debug adapter:
+    response.body = response.body || {};
+    // the adapter implements the configurationDone request.
+    response.body.supportsConfigurationDoneRequest = true;
+    // make VS Code use 'evaluate' when hovering over source
+    response.body.supportsEvaluateForHovers = true;
+    // make VS Code show a 'step back' button
+    response.body.supportsStepBack = true;
+    // make VS Code support data breakpoints
+    response.body.supportsDataBreakpoints = true;
+    // make VS Code support completion in REPL
+    response.body.supportsCompletionsRequest = true;
+    response.body.completionTriggerCharacters = [".", "["];
+    // make VS Code send cancel request
+    response.body.supportsCancelRequest = true;
+    // make VS Code send the breakpointLocations request
+    response.body.supportsBreakpointLocationsRequest = true;
+    // make VS Code provide "Step in Target" functionality
+    response.body.supportsStepInTargetsRequest = true;
+    // the adapter defines two exceptions filters, one with support for conditions.
+    response.body.supportsExceptionFilterOptions = true;
+    response.body.exceptionBreakpointFilters = [
+      {
+        filter: "namedException",
+        label: "Named Exception",
+        description: `Break on named exceptions. Enter the exception's name as the Condition.`,
+        default: false,
+        supportsCondition: true,
+        conditionDescription: `Enter the exception's name`,
+      },
+      {
+        filter: "otherExceptions",
+        label: "Other Exceptions",
+        description: "This is a other exception",
+        default: true,
+        supportsCondition: false,
+      },
+    ];
+    // make VS Code send exceptionInfo request
+    response.body.supportsExceptionInfoRequest = true;
+    // make VS Code send setVariable request
+    response.body.supportsSetVariable = false;
+    // make VS Code send setExpression request
+    response.body.supportsSetExpression = true;
+    // make VS Code send disassemble request
+    response.body.supportsDisassembleRequest = true;
+    response.body.supportsSteppingGranularity = true;
+    response.body.supportsInstructionBreakpoints = true;
+    this.sendResponse(response);
+    // since this debug adapter can accept configuration requests like 'setBreakpoint' at any time,
+    // we request them early by sending an 'initializeRequest' to the frontend.
+    // The frontend will end the configuration sequence by calling 'configurationDone' request.
+    this.sendEvent(new vscodeDebugAdapter.InitializedEvent());
+  }
+
+  /**
+   * Called at the end of the configuration sequence.
+   * Indicates that all breakpoints etc. have been sent to the DA and that the 'launch' can start.
+   * @param {DebugProtocol.ConfigurationDoneResponse} response
+   * @param {DebugProtocol.ConfigurationDoneArguments} args
+   * @returns {void}
+   */
+  configurationDoneRequest(response, args) {
+    super.configurationDoneRequest(response, args);
+    // notify the launchRequest that configuration has finished
+    this._configurationDone.notify();
   }
 }
 
