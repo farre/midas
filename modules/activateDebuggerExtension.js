e24f991d9d40431daf05b475948a506e03f4bafd
Simon Farre <simon.farre.cx@gmail.com>
2021-11-04 14:35:07 +0100

    Another move from laptop -> desktop

--- modules/activateDebuggerExtension.js
+++ modules/activateDebuggerExtension.js
@@ -33,6 +33,12 @@ function activateExtension(context, descriptorFactory) {
       vscode.DebugConfigurationProviderTriggerKind.Dynamic
     )
   );
+  // TODO(simon): when we've implemented the most bare bones debugger
+  //  meaning, we can start gdb, launch a program and stop on entry
+  //  we need to implement some "frontend" functionality,
+  //  such as, "what happens when the user hoves on a variable in the text editor?"
+  //  we do that by adding subscriptions to the context, by using functions like
+  //  vscode.languages.registerEvaluatableExpressionProvider(...)
   if (!descriptorFactory) {
     descriptorFactory = new RRDebugAdapterFactory();
   }
--- modules/debuggerInterface.js
+++ modules/debuggerInterface.js
@@ -2,9 +2,36 @@
 // pull in EventEmitter, which DebugInterface extends
 // it is through this, we dispatch communication between VSCode and GDB/MI
 const {EventEmitter} = require("events");
+const { GDB } = require("gdb-js");
+const { spawn } = require("child_process");
 
 class GDBInterface extends EventEmitter {
+  /** @type GDB */
+  #gdb;
 
+  constructor() {
+    super();
+  }
+  /** Starts debugging of executable `program`
+   * @param { string} program
+   * @param { boolean } stopOnEntry
+   * @param { boolean } debug
+   */
+  async start(program, stopOnEntry, debug) {
+    let gdb_process = spawn("gdb", ["-i=mi3", program]);
+    this.#gdb = new GDB(gdb_process);
+  }
+
+  /**
+   *
+   * @param { boolean } reverse
+   */
+  async continue(reverse = false) {
+    this.#gdb.execMI("-e")
+    return new Promise((resolve, reject) => {
+
+    })
+  }
 }
 
 module.exports = {
