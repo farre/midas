7dcb7cd6d92163ebcb8e75b834e8214732f9984b
Simon Farre <simon.farre.cx@gmail.com>
2021-11-06 00:57:16 +0100

    Working on how to track variables

--- test/cppworkspace/testrrdbg/src/main.cpp
+++ test/cppworkspace/testrrdbg/src/main.cpp
@@ -1,16 +1,24 @@
-#include <todo.hpp>
 #include <iostream>
+#include <todo.hpp>
 #include <vector>
 
-int main(int argc, const char** argv) {
-	std::vector<Todo> todos;
-	todos.push_back(Todo{"Make test app for debugger extension", Date{.day = 3, .month = 11, .year = 2021 }});
-	todos.push_back(Todo{"Read code-debug & look for useful stuff", Date{.day = 4, .month = 11, .year = 2021 }});
-	todos.push_back(Todo{"Read vscode-mock-debug & rip out things of use", Date{.day = 5, .month = 11, .year = 2021 }});
+int main(int argc, const char **argv) {
+  const auto somelocal = 42;
+  Todo tmp{"Test local struct", Date{.day = 3, .month = 11, .year = 2021}};
 
-	std::cout << "Things to do: " << Todo::todo_count() << std::endl;
-	for(const auto& t : todos) {
-		std::cout << "\tTodo id " << t.id() << ": " << t.title() << " @" << t.date() << std::endl;
-	}
-	std::cout << "Goodbye cruel world" << std::endl;
+  std::vector<Todo> todos{};
+  todos.push_back(Todo{"Make test app for debugger extension",
+                       Date{.day = 3, .month = 11, .year = 2021}});
+  todos.push_back(Todo{"Read code-debug & look for useful stuff",
+                       Date{.day = 4, .month = 11, .year = 2021}});
+  todos.push_back(Todo{"Read vscode-mock-debug & rip out things of use",
+                       Date{.day = 5, .month = 11, .year = 2021}});
+
+  std::cout << "Things to do: " << Todo::todo_count() << std::endl;
+  for (const auto &t : todos) {
+    std::cout << "\tTodo id " << t.id() << ": " << t.title() << " @" << t.date()
+              << std::endl;
+  }
+  const auto foo = "foobar";
+  std::cout << "Goodbye cruel world" << std::endl;
 }
