dbd8cf172f8678e75b1f5853586611f5c884c226
Simon Farre <simon.farre.cx@gmail.com>
2021-11-07 15:53:21 +0100

    Added some template code to the cppworkspace
    This is just so that in the future, we will be able
    to implement features for handling templated types.
    
    GDB should handle these things for us, but one never knows.

--- test/cppworkspace/testrrdbg/src/main.cpp
+++ test/cppworkspace/testrrdbg/src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
-#include <todo.hpp>
 #include <vector>
+
+#include <number.hpp>
+#include <todo.hpp>
+
+template <IsNumber Num> Number<Num> add_two(Num a, Num b) {
+  Number l{a};
+  Number r{b};
+  return Number<Num>::sum(l, r);
+}
 
 int main(int argc, const char **argv) {
   const auto somelocal = 42;
   Todo tmp{"Test local struct", Date{.day = 3, .month = 11, .year = 2021}};
+  auto Double = add_two(1.550795, 1.590795);
+  auto Float = add_two(668.19685f, 668.93685f);
+  auto Int = add_two(20, 22);
+
+  std::cout << "Value of " << Double << std::endl;
+  std::cout << "Value of " << Float << std::endl;
+  std::cout << "Value of " << Int << std::endl;
 
   std::vector<Todo> todos{};
   todos.push_back(Todo{"Make test app for debugger extension",
@@ -19,6 +34,10 @@ int main(int argc, const char **argv) {
     std::cout << "\tTodo id " << t.id() << ": " << t.title() << " @" << t.date()
               << std::endl;
   }
-  const auto foo = "foobar";
+  // lets be longer than a machine register
+  static const auto foo = "foobar is something to say";
+  static constexpr auto bar = "saying barfoo is something nobody does";
+  constexpr auto baz = "baz is also kind of a cool word!!!!!!!!!!!!!!!";
+  constexpr const char *bazchar = "These types end up being wildly different";
   std::cout << "Goodbye cruel world" << std::endl;
 }
