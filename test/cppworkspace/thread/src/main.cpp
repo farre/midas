02e3d64cab55d2e62b27ad9b4172a1a14dee116d
Simon Farre <simon.farre.cx@gmail.com>
2021-11-10 14:46:48 +0100

    Added another cpp project to test against
    This project will be used for testing things
    involving threads (and also pausing execution
    and in order to do this, we'll have to put the
    "this thread" to sleep).

---
+++ test/cppworkspace/thread/src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+
+#include <number.hpp>
+#include <todo.hpp>
+
+template <IsNumber Num> Number<Num> add_two(Num a, Num b) {
+  Number l{a};
+  Number r{b};
+  return Number<Num>::sum(l, r);
+}
+
+int main(int argc, const char **argv) {
+  using namespace std::chrono_literals;
+  const auto somelocal = 42;
+  Todo tmp{"Test local struct", Date{.day = 3, .month = 11, .year = 2021}};
+  auto Double = add_two(1.550795, 1.590795);
+  auto Float = add_two(668.19685f, 668.93685f);
+  auto Int = add_two(20, 22);
+
+  std::cout << "Value of " << Double << std::endl;
+  std::cout << "Value of " << Float << std::endl;
+  std::cout << "Value of " << Int << std::endl;
+
+  std::vector<Todo> todos{};
+  todos.push_back(Todo{"Make test app for debugger extension",
+                       Date{.day = 3, .month = 11, .year = 2021}});
+  todos.push_back(Todo{"Read code-debug & look for useful stuff",
+                       Date{.day = 4, .month = 11, .year = 2021}});
+  todos.push_back(Todo{"Read vscode-mock-debug & rip out things of use",
+                       Date{.day = 5, .month = 11, .year = 2021}});
+
+  std::cout << "Things to do: " << Todo::todo_count() << std::endl;
+  for (const auto &t : todos) {
+    std::cout << "\tTodo id " << t.id() << ": " << t.title() << " @" << t.date()
+              << std::endl;
+  }
+
+  // so that we can test pausing execution, for instance.
+  std::this_thread::sleep_for(1000ms);
+
+  // lets be longer than a machine register
+  static const auto foo = "foobar is something to say";
+  static constexpr auto bar = "saying barfoo is something nobody does";
+  constexpr auto baz = "baz is also kind of a cool word!!!!!!!!!!!!!!!";
+  constexpr const char *bazchar = "These types end up being wildly different";
+  std::cout << "Goodbye cruel world" << std::endl;
+}
