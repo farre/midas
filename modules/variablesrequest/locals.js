4501c3a1eedc2fba21a79cef6f723e0f64f1a94a
Simon Farre <simon.farre.cx@gmail.com>
2022-01-14 17:38:01 +0100

    fixed a bug, that gets introduced when we try to track a lambda as a variable. A lambda, unlike closures (or I supposed in this context, a non-capturing lambda?) have no children or value. So VSCode would think it was a structured type, when it's just a function pointer.

--- modules/variablesrequest/locals.js
+++ modules/variablesrequest/locals.js
@@ -32,15 +32,23 @@ class LocalsReference extends VariablesReference {
         let vscodeRef = 0;
         const voname = `vr_${nextRef}`;
         let cmd = `-var-create ${voname} * ${name}`;
-        if (!value) {
+
+        // we have to execute the creation of varObjs first; because if we have come across a lambda (i.e not closures, they capture something, so that will be fine)
+        // it will *not* have `value` set, like structured types, but it will also *not* have numchild > 0, so we must find out this first, so
+        // we don't add lambdas to variables we should track.
+        let numchild = await gdb_.execMI(cmd, this.threadId).then((res) => res.numchild);
+        if (!value && numchild > 0) {
           vscodeRef = nextRef;
           gdb_.references.set(nextRef, new StructsReference(nextRef, this.threadId, this.frameLevel, voname));
           gdb_.getExecutionContext(this.threadId).addTrackedVariableReference({ id: nextRef, isChild: false });
+        } else if (!value && numchild == 0) {
+          await gdb_.execMI(`-var-delete ${voname}`, this.threadId);
+          gdb_.varRefContexts.delete(nextRef);
+          continue;
         }
 
         let mvar = new GDB.MidasVariable(name, value ?? type, vscodeRef, voname, value ? false : true);
         this.#variables.push(mvar);
-        await gdb_.execMI(cmd, this.threadId);
       }
       response.body = {
         variables: this.#variables,
