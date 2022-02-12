462b762b47d2d6597c6d1a1492a0b42226c4c1c1
Simon Farre <simon.farre.cx@gmail.com>
2022-02-12 02:20:22 +0100

    statics scopes added as well as base class

--- modules/python/stackFrameState.py
+++ modules/python/stackFrameState.py
@@ -1,13 +1,255 @@
+from xml.dom.expatbuilder import parseString
 import gdb
 import sys
 import json
 import gdb.types
 import traceback
 import logging
+import time
+
+# from .utils import getMembersRecursively, memberIsReference, selectThreadAndFrame, parseStringArgs, getFunctionBlock, static_display, typeIsPrimitive, display, prepareOutput, logExceptionBacktrace
 
 logging.basicConfig(filename='update.log', filemode="w", encoding='utf-8', level=logging.DEBUG)
 
-staticsTracker = {}
+def getFunctionBlock(frame) -> gdb.Block:
+    block = frame.block()
+    while not block.superblock.is_static and not block.superblock.is_global:
+        block = block.superblock
+    if block.is_static or block.is_global:
+        return None
+    return block
+
+def logExceptionBacktrace(errmsg, exception):
+        logging.error("{} Exception info: {}".format(errmsg, exception))
+        logging.error(traceback.format_exc())
+
+def selectThreadAndFrame(threadId, frameLevel):
+    try:
+        gdb.execute("thread {}".format(threadId))
+        gdb.execute("frame {}".format(frameLevel))
+    except Exception as e:
+        logExceptionBacktrace("Selecting thread and frame failed.", e)
+
+def parseStringArgs(arg):
+    return gdb.string_to_argv(arg)
+
+def prepareOutput(cmdName, contents):
+    return '<gdbjs:cmd:{0} {1} {0}:cmd:gdbjs>'.format(cmdName, contents)
+
+def typeIsPrimitive(valueType):
+    try:
+        for f in valueType.fields():
+            if hasattr(f, "enumval"):
+                return True
+            else:
+                return False
+    except TypeError:
+        return True
+
+def memberIsReference(type):
+    code = type.code
+    return code == gdb.TYPE_CODE_PTR or code == gdb.TYPE_CODE_REF or code == gdb.TYPE_CODE_RVALUE_REF
+
+def getMembersRecursively(field, memberList, statics):
+    if field.bitsize > 0:
+        logging.info("field {} is possibly a bitfield of size {}".format(field.name, field.bitsize))
+    if hasattr(field, 'bitpos'):
+        if field.is_base_class:
+            for f in field.type.fields():
+                getMembersRecursively(f, memberList=memberList, statics=statics)
+        else:
+            if field.name is not None and not field.name.startswith("_vptr"):
+                memberList.append(field.name)
+    else:
+        statics.append(field.name)
+
+def getMembers(field, memberList, statics, baseclasses):
+    if hasattr(field, 'bitpos') and field.name is not None and not field.name.startswith("_vptr") and not field.is_base_class:
+        memberList.append(field.name)
+    elif field.is_base_class:
+        baseclasses.append(field.name)
+    else:
+        statics.append(field.name)
+
+
+def getValue(value):
+    print("Trying to get value of {0}".format(value))
+    if memberIsReference(value.type):
+        try:
+            v = value.referenced_value()
+            return v
+        except gdb.MemoryError:
+            return value
+    else:
+        return value
+
+def getElement(key, map):
+    try:
+        r = map[key]
+        return r
+    except KeyError:
+        return None
+
+def getMemberValue(path):
+    pathComponents = path.split(".")
+    parent = pathComponents[0]
+    try:
+        it = gdb.parse_and_eval(parent)
+        pathComponents = pathComponents[1:]
+        if len(pathComponents):
+            return it
+        for path in pathComponents:
+            curr = getElement(path, it)
+            if curr is None:
+                return None
+            it = curr
+        
+        return it
+    except gdb.error:
+        return None
+
+def display(name, value, isPrimitive):
+    try:
+        if value.type.code == gdb.TYPE_CODE_PTR:
+            if isPrimitive:
+                return { "name": name, "display": "<{}> {}".format(value.dereference().address, value), "isPrimitive": isPrimitive, "static": False }
+            else:
+                return { "name": name, "display": "<{}> {}".format(value.dereference().address, value.type), "isPrimitive": isPrimitive, "static": False }
+        else:
+            if isPrimitive:
+                return { "name": name, "display": "{}".format(value), "isPrimitive": isPrimitive, "static": False }
+            else:
+                return { "name": name, "display": "{}".format(value.type), "isPrimitive": isPrimitive, "static": False }
+    except:
+        return { "name": name, "display": "<invalid address> {}".format(value.type), "isPrimitive": isPrimitive, "static": False }
+
+def base_class_display(name, type):
+    return { "name": name, "display": "{} (base)".format(type) }
+
+def static_display(name, type):
+    isPrimitive = True if type.tag is None else False
+    typeName = type.tag if type.tag is not None else type
+    return { "name": name, "display": "static {}".format(typeName), "isPrimitive": isPrimitive, "static": True }
+
+class ContentsOfStatic(gdb.Command):
+    def __init__(self):
+        super(ContentsOfStatic, self).__init__("gdbjs-get-contents-of-static", gdb.COMMAND_USER)
+        self.name = "get-contents-of-static"
+    
+    def invoke(self, arguments, from_tty):
+        [threadId, frameLevel, expression] = parseStringArgs(arguments)
+        selectThreadAndFrame(threadId=threadId, frameLevel=frameLevel)
+        components = expression.split(".")
+        it = gdb.parse_and_eval(components[0])
+
+        for component in components[1:]:
+            it = it[component]
+        if it.type.code == gdb.TYPE_CODE_PTR:
+            it = it.dereference()
+        try:
+            if memberIsReference(it.type):
+                it = it.referenced_value()
+        except Exception as e:
+            logging.error("Couldn't dereference value {}".format(expression))
+            raise e
+        result = []
+        try:
+            members = []
+            statics = []
+            fields = it.type.fields()
+            for f in fields:
+                getMembersRecursively(f, memberList=members, statics=statics)
+            result = []
+
+            for member in members:
+                item = display(member, it[member], typeIsPrimitive(it[member].type))
+                result.append(item)
+
+            for static in statics:
+                item = static_display(static, it.type[static].type)
+                result.append(item)
+        except:
+            result.append({ "name": "static value", "display": "{}".format(it), "isPrimitive": True, "static": True })
+        res = json.dumps(result, ensure_ascii=False)
+        msg = prepareOutput(self.name, res)
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+
+contentsOfStaticCommand = ContentsOfStatic()
+
+class ContentsOfBaseClass(gdb.Command):
+    def __init__(self):
+        super(ContentsOfBaseClass, self).__init__("gdbjs-get-contents-of-base-class", gdb.COMMAND_USER)
+        self.name = "get-contents-of-base-class"
+    
+    def invoke(self, arguments, from_tty):
+        logging.info("Arguments: {}".format(arguments))
+        [threadId, frameLevel, expression, base_classes] = parseStringArgs(arguments)
+        selectThreadAndFrame(threadId=threadId, frameLevel=frameLevel)
+        components = expression.split(".")
+        base_classes = parseStringArgs(base_classes)
+        logging.info("parsing expression '{}' and it's baseclasses: {}".format(expression, base_classes))
+        it = gdb.parse_and_eval(components[0])
+        for c in components[1:]:
+            it = it[c]
+
+        if it.type.code == gdb.TYPE_CODE_PTR:
+            it = it.dereference()
+        try:
+            if memberIsReference(it.type):
+                it = it.referenced_value()
+        except:
+            logging.error("Couldn't dereference value {}".format(expression))
+        
+        members = []
+        statics = []
+        baseclasses = []
+        fields = []
+        result = {"members": [], "statics": [], "base_classes": [ ]}
+        currentBc = None
+        currentTypeName = None
+        try:
+            typeIterator = it.type
+            for bc in map(lambda bc: bc.replace("_*_*_", " "), base_classes):
+                # --- debug purposes ---
+                currentBc = bc
+                currentTypeName = typeIterator.name
+                # /// debug purposes ///
+                typeIterator = typeIterator[bc].type
+                it = it.cast(typeIterator)
+
+            fields = typeIterator.fields()
+            for f in fields:
+                getMembers(f, memberList=members, statics=statics, baseclasses=baseclasses)
+            
+            for member in members:
+                item = display(member, it[member], typeIsPrimitive(it[member].type))
+                result["members"].append(item)
+            
+            for static in statics:
+                item = static_display(static, it.type[static].type)
+                result["statics"].append(item)
+
+            for baseclass in baseclasses:
+                item = base_class_display(baseclass, it.type[baseclass].type)
+                result["base_classes"].append(item)
+            
+            res = json.dumps(result, ensure_ascii=False)
+            msg = prepareOutput(self.name, res)
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+        except Exception as e:
+            extype, exvalue, extraceback = sys.exc_info()
+            fieldsNames = []
+            for f in fields:
+                fieldsNames.append("{}".format(f.name))
+            logging.error("Current type {} -> couldn't go to next type {}. Exception: {} | {}".format(currentTypeName, currentBc, extype, exvalue))
+            res = json.dumps(None, ensure_ascii=False)
+            msg = prepareOutput(self.name, res)
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+
+contentsOfBaseClassCommand = ContentsOfBaseClass()
 
 class ContentsOf(gdb.Command):
     def __init__(self):
@@ -18,9 +260,8 @@ class ContentsOf(gdb.Command):
         [threadId, frameLevel, expression] = parseStringArgs(arguments)
         selectThreadAndFrame(threadId=threadId, frameLevel=frameLevel)
         components = expression.split(".")
-        it = gdb.parse_and_eval(components[0])            
-        components = components[1:]
-        for component in components:
+        it = gdb.parse_and_eval(components[0])
+        for component in components[1:]:
             it = it[component]
         if it.type.code == gdb.TYPE_CODE_PTR:
             it = it.dereference()
@@ -34,30 +275,41 @@ class ContentsOf(gdb.Command):
 
         members = []
         statics = []
-        fields = it.type.fields()
-        for f in fields:
-            getMembersRecursively(f, members, statics=statics)
-        result = []
-        # register the static members we've seen
-        if staticsTracker.get(components[0]) is None:
-            staticsTracker[components[0]] = {}
-        iter = staticsTracker[components[0]]
-        for component in components[1:]:
-            if iter.get(component) is None:
-                iter[component] = { "statics": None }
-            iter = iter[component]
-        
-        if len(statics) != 0:
-            iter["statics"] = statics
-        
-        for member in members:
-            item = display(member, it[member], typeIsPrimitive(it[member].type))
-            result.append(item)
-        
-        res = json.dumps(result, ensure_ascii=False)
-        msg = prepareOutput(self.name, res)
-        sys.stdout.write(msg)
-        sys.stdout.flush()
+        baseclasses = []
+        fields = []
+        result = {"members": [], "statics": [], "base_classes": [ ]}
+        try:
+            fields = it.type.fields()
+            for f in fields:
+                getMembers(f, memberList=members, statics=statics, baseclasses=baseclasses)
+            
+            for member in members:
+                item = display(member, it[member], typeIsPrimitive(it[member].type))
+                result["members"].append(item)
+            
+            for static in statics:
+                item = static_display(static, it.type[static].type)
+                result["statics"].append(item)
+
+            for baseclass in baseclasses:
+                item = base_class_display(baseclass, it.type[baseclass].type)
+                result["base_classes"].append(item)
+            
+            res = json.dumps(result, ensure_ascii=False)
+            msg = prepareOutput(self.name, res)
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+        except Exception as e:
+            extype, exvalue, extraceback = sys.exc_info()
+            fieldsNames = []
+            for f in fields:
+                fieldsNames.append("{}".format(f.name))
+            logging.error("Couldn't retrieve contents of {}. Exception type: {} - Exception value: {}. Fields: {}\nRecursively found members: {} \t statics: {}\n".format(expression, extype, exvalue, fieldsNames, members, statics))
+            res = json.dumps(None, ensure_ascii=False)
+            msg = prepareOutput(self.name, res)
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+
 
 updatesOfCommand = ContentsOf()
 
---
+++ modules/variablesrequest/statics.js
@@ -0,0 +1,116 @@
+const GDB = require("../gdb");
+const { StructsReference } = require("./structs");
+const { VariablesReference, err_response } = require("./variablesReference");
+
+let LOG_ID = 0;
+function log(reason, message) {
+  if (!GDB.trace) {
+    return;
+  }
+  console.log(`[LOG #${LOG_ID++}: ${reason}] - ${message}`);
+}
+
+/**
+ * @typedef { import("@vscode/debugprotocol").DebugProtocol.VariablesResponse } VariablesResponse
+ * @typedef { import("@vscode/debugprotocol").DebugProtocol.SetVariableResponse } SetVariableResponse
+ * @typedef { import("../gdb").GDB } GDB
+ * @typedef { import("../executionState").ExecutionState } ExecutionState
+ * @typedef { import("../gdb").VSCodeVariable } VSCodeVariable
+ */
+
+class StaticsReference extends VariablesReference {
+  /** @type {string} */
+  evaluateName;
+  subStructs = new Map();
+  namesRegistered = new Map();
+  stackFrameIdentifier;
+  initialized = false;
+
+  /**
+   * 
+   * @param { number } variablesReference 
+   * @param { number } threadId 
+   * @param { number } frameLevel 
+   * @param { string } evaluateName
+   * @param { number } stackFrameIdentifier 
+   */
+  constructor(variablesReference, threadId, frameLevel, evaluateName, stackFrameIdentifier) {
+    super(variablesReference, threadId, frameLevel);
+    this.stackFrameIdentifier = stackFrameIdentifier;
+    this.evaluateName = evaluateName;
+  }
+
+  /**
+   * @param { VariablesResponse } response - response which we prepare, to be sent back to VSCode
+   * @param { GDB } gdb - reference to the GDB backend
+   * @returns { Promise<VariablesResponse> }
+   */
+  async handleRequest(response, gdb) {
+    let result = []
+    let children = await gdb.getContentsOfStatic(this.threadId, this.frameLevel, this.evaluateName);
+    for(const child of children) {
+      const path = `${this.evaluateName}.${child.name}`;
+      if(child.isPrimitive) {
+        let v = new GDB.VSCodeVariable(child.name, child.display, 0, path, false, path);
+        result.push(v);
+      } else {
+        let nextRef = this.namesRegistered.get(child.name);
+        if(!nextRef) {
+          nextRef = gdb.generateVariableReference();
+          let subStructHandler = new StructsReference(nextRef, this.threadId, this.frameLevel, path, this.stackFrameIdentifier);
+          gdb.references.set(
+            nextRef,
+            subStructHandler
+          );
+          this.namesRegistered.set(child.name, nextRef);
+          gdb.getExecutionContext(this.threadId).addTrackedVariableReference(nextRef, this.stackFrameIdentifier);
+        }
+        let v = new GDB.VSCodeVariable(child.name, child.display, nextRef, path, true, path);
+        result.push(v);
+      }
+    }
+    response.body = {
+      variables: result
+    }
+    return response;
+  }
+
+  async cleanUp(gdb) {
+    // we don't need to do clean up; we're always managed by either a LocalsReference or a WatchReference
+    gdb.references.delete(this.variablesReferenceId);
+    for(const [name, ref] of this.namesRegistered) {
+      gdb.references.delete(ref);
+    }
+  }
+
+  /**
+   * Sets a new value of a named object (variable object) that this reference tracks or manages.
+   * @param { SetVariableResponse } response - The response initialized by VSCode which we should return
+   * @param {GDB} gdb - GDB backend instance
+   * @param {string} namedObject - a named object's name, that this VariablesReference tracks, which should be updated
+   * @param {string} value - The `value` in string form which the named object should be updated to hold
+   * @returns { Promise<SetVariableResponse> } prepared VSCode response
+   */
+  async update(response, gdb, namedObject, value) {
+    const vo_name = `ASSIGN_TEMPORARY_${this.evaluateName}_${namedObject}`;
+    let res = await gdb.execMI(`-var-create ${vo_name} * ${this.evaluateName}.${namedObject}`);
+    try {
+      res = await gdb.execMI(`-var-assign ${vo_name} "${value}"`, this.threadId);
+      await gdb.execMI(`-var-delete ${vo_name}`);
+      if (res.value) {
+        response.body = {
+          value: res.value
+        };
+        return response;
+      }
+    } catch(err) {
+      await gdb.execMI(`-var-delete ${vo_name}`);
+      return err_response(response, `${namedObject} is not editable`);
+    }
+  }
+
+}
+
+module.exports = {
+  StaticsReference,
+};
