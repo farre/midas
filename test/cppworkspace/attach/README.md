65af9d6f75662b8d106a1fc4913a0430e4cecfb9
Simon Farre <simon.farre.cx@gmail.com>
2022-03-28 17:16:39 +0200

    attach test workspace

---
+++ test/cppworkspace/attach/README.md
@@ -0,0 +1,11 @@
+# For testing of GDB
+
+Building a debug build with cmake:
+
+```bash
+# While in test folder
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake --build .
+```
---
+++ test/cppworkspace/attach/src/main.cpp
@@ -0,0 +1,15 @@
+#include <csignal>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <unistd.h>
+
+int main(int argc, const char** argv) {
+  std::cout << "Hello world" << std::endl;
+  auto pid = getpid();
+  std::cout << pid << std::endl;
+  // let's interrupt here, so that we are given time to attach with gdb
+  std::this_thread::sleep_for(std::chrono::seconds(45));
+  std::cout << "swoosh" << std::endl;
+}
