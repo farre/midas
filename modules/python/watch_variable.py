90463ac98adc57ebea733f61e254691fb3d01f65
Simon Farre <simon.farre.cx@gmail.com>
2022-03-28 13:31:45 +0200

    added watch variable

--- modules/python/watch_variable.py
+++ modules/python/watch_variable.py
@@ -1,25 +1,61 @@
-from pydoc import resolve
 import gdb
+import config
+import midas_utils
 
-import config as config
-from midas_utils import parse_command_args, timeInvocation, getClosest, resolve_gdb_value
+"""
+        body: {
+            result: string;
+            type?: string;
+            variablesReference: number;
+            namedVariables?: number;
+            indexedVariables?: number;
+            memoryReference?: string;
+        }
+"""
+
+def response(success, message, result, type=None, variableReference=0, namedVariables=None, indexedVariables=None, memoryReference=None):
+    return {
+            "body":
+                {
+                    "result": result,
+                    "type": type,
+                    "variablesReference": variableReference,
+                    "namedVariables": namedVariables,
+                    "indexedVariables": indexedVariables,
+                    "memoryReference": memoryReference
+                },
+                "success": success,
+                "message": message
+            }
 
 class WatchVariable(gdb.Command):
     """Not to be confused with watch point."""
-    def __init__(self):
+    def __init__(self, executionContexts):
         super(WatchVariable, self).__init__("gdbjs-watch-variable", gdb.COMMAND_USER)
         self.name = "watch-variable"
+        self.executionContexts = executionContexts
 
-    @timeInvocation
+    @config.timeInvocation
     def invoke(self, args, from_tty):
-        [expr, threadId, frameLevel] = parse_command_args(args)
-        (thread, frame) = config.currentExecutionContext.set_context(threadId=threadId, frameLevel=frameLevel)
-        components = expr.split(".")
-        it = getClosest(frame, components[0])
-        it = resolve_gdb_value(it, components=components[1:])
-        pp = gdb.default_visualizer(it)
-        if pp is None:
-            0
-        else:
-            0
-
+        try:
+            [expr, frameId] = midas_utils.parse_command_args(args, str, int)
+            refId = config.variableReferences.get_context(frameId)
+            if refId is None:
+                raise gdb.GdbError("No variable reference mapping for frame id {} exists".format(frameId))
+            ec = self.executionContexts.get(refId.threadId)
+            if ec is None:
+                raise gdb.GdbError("Execution context does not exist")
+            frame = ec.set_known_context(frameId)
+            components = expr.split(".")
+            it = midas_utils.get_closest(frame, components[0])
+            if it is None:
+                midas_utils.send_response(self.name,response(result="no symbol with that name in context", success=False, message="could not evaluate"), midas_utils.prepare_command_response)
+            else:
+                sf = ec.get_stackframe(frameId)
+                v = sf.add_watched_variable(expr, it)
+                res = v.to_vs()
+                result = response(success=True, message=None, result=res["value"], type="{}".format(v.get_type()), variableReference=v.get_variable_reference())
+                midas_utils.send_response(self.name, result, midas_utils.prepare_command_response)
+        except Exception as e:
+            config.log_exception(config.error_logger(), "{} failed: {}".format(self.name, e), e)
+            midas_utils.send_response(self.name, response(success=False, message="Could not be evaluated", result=None), midas_utils.prepare_command_response)
