9c4368299fe1a52eec927bd6751db5b93d363a4e
Simon Farre <simon.farre.cx@gmail.com>
2022-04-21 18:39:25 +0200

    added dataBreakpointInfoRequest

---
+++ modules/python/data_breakpoint_info_request.py
@@ -0,0 +1,42 @@
+import gdb
+import midas_utils
+import config
+
+# Resolves full evaluate path since setDataBreakpointRequest has a pretty bad design flaw
+
+
+class DataBreakpointInfoRequest(gdb.Command):
+
+    def __init__(self, executionContexts):
+        super(DataBreakpointInfoRequest, self).__init__("gdbjs-data-breakpoint-info", gdb.COMMAND_USER)
+        self.name = "data-breakpoint-info"
+        self.executionContexts = executionContexts
+
+    def invoke(self, args, from_tty):
+        [name, variableReference] = midas_utils.parse_command_args(args, str, int)
+        refId = config.variableReferences.get_context(variableReference)
+        if refId is None:
+            # todo(simon): this is where we start checking for watch variables, as they are context-independent and re-evaluated in every context that gets selected
+            raise gdb.GdbError("No refId referenced by {} exists".format(variableReference))
+        ec = self.executionContexts.get(refId.threadId)
+        if ec is None:
+            raise gdb.GdbError("No execution context is referencing {}".format(variableReference))
+        sf = ec.get_stackframe(refId.frameId)
+        v = sf.get_variable(variableReference)
+        dataId = None
+        if v is not None:
+            dataId = "{}.{}".format(v.evaluateName, name)
+        body = {
+            "dataId":
+            dataId,
+            "description":
+            "Could not resolve {} for variable referece {}".format(name, variableReference) if v is None else "",
+            "accessTypes": [
+                "read",
+                "write",
+                "readWrite",
+            ],
+            "canPersist":
+            False
+        }
+        midas_utils.send_response(self.name, body, midas_utils.prepare_command_response)
