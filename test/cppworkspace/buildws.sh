b0ebb069cf21bb72c8aa975539dabe9e1f80ceac
Simon Farre <simon.farre.cx@gmail.com>
2022-05-04 15:46:34 +0200

    added debug / relwithdebinfo building for test cases
    also added test case for inlining so that testing of breakpoints
    in inlined functions is possible.

--- test/cppworkspace/buildws.sh
+++ test/cppworkspace/buildws.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-
+last_build_pid=""
 projects=("test" "thread" "simple_input" "attach" "rr")
 for project in "${projects[@]}"; do
-    echo "building $project ..."
+    echo "building $project in Debug and RelWithDebInfo CMake modes..."
     cd "$project"
-    mkdir build
-    cd build
-    cmake .. -DCMAKE_BUILD_TYPE=Debug
-    cmake --build . --config Debug -- -j 15 
-    cd ../..
+    mkdir -p build/debug
+    mkdir -p build/release
+    cd build/debug
+    cmake ../.. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+    cmake --build . --config Debug &
+    cd ../release
+    cmake ../.. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+    cmake --build . --config RelWithDebInfo &
+    last_build_pid="${last_build_pid} $!"
+    cd ../../..
 done
+# wait for all builds to finish.
+wait $last_build_pid
+echo ""
+echo -e "[\x1b[1;44m\x1b[1;32m --- Building all test cases in Debug and RelWithDebInfo: Done --- \x1b[m]"
--- test/cppworkspace/test/src/main.cpp
+++ test/cppworkspace/test/src/main.cpp
@@ -1,39 +1,25 @@
-#include <iostream>
-#include <iterator>
-#include <vector>
-#include <string>
-#include <number.hpp>
-#include "testcase_namespaces/test_ptrs.hpp"
-#include "testcase_namespaces/enum.hpp"
 #include "testcase_namespaces/baseclasses.hpp"
+#include "testcase_namespaces/derive.hpp"
+#include "testcase_namespaces/enum.hpp"
 #include "testcase_namespaces/longstack.hpp"
+#include "testcase_namespaces/pp.hpp"
 #include "testcase_namespaces/statics.hpp"
 #include "testcase_namespaces/structrequests.hpp"
-#include "testcase_namespaces/derive.hpp"
-#include "testcase_namespaces/pp.hpp"
+#include "testcase_namespaces/test_ptrs.hpp"
+#include <iostream>
+#include <iterator>
+#include <number.hpp>
+#include <string>
+#include <vector>
 
 // Singly linked list node mixin
-template<typename T>
-struct intrusive_list_node {
-  T* next = nullptr;
-};
+template <typename T> struct intrusive_list_node { T *next = nullptr; };
 
-int overload(int a)
-{
-  return a * 2;
-}
-float overload(float a)
-{
-  return a * 2.0f;
-}
-double overload(double a)
-{
-  return a * 2.0;
-}
+int overload(int a) { return a * 2; }
+float overload(float a) { return a * 2.0f; }
+double overload(double a) { return a * 2.0; }
 
-template <IsNumber Num>
-Number<Num> add_two(Num a, Num b)
-{
+template <IsNumber Num> Number<Num> add_two(Num a, Num b) {
   Number l{a};
   Number r{b};
   return Number<Num>::sum(l, r);
@@ -50,7 +36,7 @@ struct T : intrusive_list_node<T> {
 };
 
 struct Ts {
-  T* ts;
+  T *ts;
 };
 
 void doFooBar() {
@@ -58,57 +44,69 @@ void doFooBar() {
   fooBar.j += 1;
 }
 
-void testRValueReferences(std::string&& item) {
+void testRValueReferences(std::string &&item) {
   auto result = item;
   std::cout << "item is: " << result;
 }
 
-int main(int argc, const char **argv)
-{
+// test case for inlined functions and breakpoints set on them.
+inline void alter_t(T &t) {
+  t.f++;
+  t.s.j++;
+  t.s.k++;
+}
+
+int main(int argc, const char **argv) {
   std::vector<std::string> foos[3]{};
 
   std::vector<std::string> captured_args;
-  std::copy(argv, argv+argc, std::back_inserter(captured_args));
-  std::copy(argv, argv+argc, std::back_inserter(foos[0]));
-  std::copy(argv, argv+argc, std::back_inserter(foos[1]));
-  std::copy(argv, argv+argc, std::back_inserter(foos[2]));
-  std::string helloworld{"Hello world, I manage myself and I'm also made sure to be allocated on the heap"};
+  std::copy(argv, argv + argc, std::back_inserter(captured_args));
+  std::copy(argv, argv + argc, std::back_inserter(foos[0]));
+  std::copy(argv, argv + argc, std::back_inserter(foos[1]));
+  std::copy(argv, argv + argc, std::back_inserter(foos[2]));
+  std::string helloworld{"Hello world, I manage myself and I'm also made sure "
+                         "to be allocated on the heap"};
   std::string_view v{helloworld};
 
   doFooBar();
   testRValueReferences(std::move(helloworld));
   T t{.s = S{.j = 10, .k = 200}, .f = 3.14};
+  [[always_inline]] alter_t(t);
   std::vector<T> vec_ts{};
   vec_ts.push_back(T{.s = S{.j = 42, .k = 5005}, .f = 13.37});
   vec_ts.push_back(T{.s = S{.j = 1, .k = 2}, .f = 3.0});
-
+  [[always_inline]] alter_t(t);
   // watch variable `stack_ts[0:2]` should produce 2 elements of T
-  T stack_ts[2]{T{.s = S{.j = 42, .k = 5005}, .f = 13.37}, T{.s = S{.j = 1, .k = 2}, .f = 3.0}};
+  T stack_ts[2]{T{.s = S{.j = 42, .k = 5005}, .f = 13.37},
+                T{.s = S{.j = 1, .k = 2}, .f = 3.0}};
   stack_ts[0].next = &stack_ts[1];
-  auto tptrs = new T*[2];
+  auto tptrs = new T *[2];
   // watch variable `tptrs[0:2]` should produce 2 elements of T*
   tptrs[0] = vec_ts.data();
-  tptrs[1] = vec_ts.data()+1;
-  // watch variable: `it[0:2]` should produce first two elements of vec_ts since they are laid out adjacent in memory
-  T* it = *tptrs;
-  // for testing that watch var subscript operators work on members; `ts.ts[0:2]` should produce what `it[0:2]` does in this case
+  tptrs[1] = vec_ts.data() + 1;
+  // watch variable: `it[0:2]` should produce first two elements of vec_ts since
+  // they are laid out adjacent in memory
+  T *it = *tptrs;
+  // for testing that watch var subscript operators work on members;
+  // `ts.ts[0:2]` should produce what `it[0:2]` does in this case
   Ts ts;
   ts.ts = vec_ts.data();
 
   const auto somelocal = 42;
-  constexpr int array[10] = {0,1,2,3,4,5,6,7,8,9};
-  constexpr S s_array[7] {{1,2}, {2,3}, {3,4}, {4,5}, {5,6}, {6,7}, {7,8}};
-  T* tptr_to_stack = stack_ts;
+  constexpr int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  constexpr S s_array[7]{{1, 2}, {2, 3}, {3, 4}, {4, 5},
+                         {5, 6}, {6, 7}, {7, 8}};
+  T *tptr_to_stack = stack_ts;
 
-  T** ptrs_to_ptr = new T*[2];
+  T **ptrs_to_ptr = new T *[2];
   ptrs_to_ptr[0] = vec_ts.data();
   ptrs_to_ptr[1] = new T{.s = S{.j = 999, .k = 888}, .f = -0.1234567};
   ptrs_to_ptr[1]->next = &vec_ts.back();
   auto iptr = new int{42};
-  int* ptrs[10];
-  int* arrayPtrs = new int[10];
-  for(auto idx = 0; idx < 10; idx++) {
-    ptrs[idx] = new int{42+idx};
+  int *ptrs[10];
+  int *arrayPtrs = new int[10];
+  for (auto idx = 0; idx < 10; idx++) {
+    ptrs[idx] = new int{42 + idx};
     arrayPtrs[idx] = idx;
   }
   t.s.j++;
