312023e5ac59c50fead0ae3db293f9d2c6580ab4
Simon Farre <simon.farre.cx@gmail.com>
2022-12-08 11:38:38 +0100

    cleaned up a bit

--- modules/utils/installerProgress.js
+++ modules/utils/installerProgress.js
@@ -25,38 +25,19 @@ function prepare_request(deps) {
   return { packet_size: packet_size, payload: payload };
 }
 
+function handle_payload(listeners, comms_payload) {
+  listeners[comms_payload.action].emit(comms_payload.type, comms_payload.data);
+}
+
 /**
- * Inits and runs installer
- * @param {string} repo_type - whether we're using apt or dnf
- * @param {string[]} pkgs - list of depedencies to install
- * @param {boolean} cancellable - Whether or not the install operation can be cancelled
+ * Creates IPC server (unix socket) that communicates with Python scripts
+ * @param { string[] } pkgs - list of packages to request install or check if dependencies are met
+ * @param { { download: EventEmitter, install: EventEmitter } } listeners - listeners that handle the logic of processed IPC payloads
+ * @returns
  */
-async function initInstaller(repo_type, pkgs, cancellable) {
-  let logger = vscode.window.createOutputChannel("Installing RR dependencies", "Log");
-  logger.show();
-  // eslint-disable-next-line max-len
-  let pass = await vscode.window.showInputBox({ prompt: "sudo password", password: true });
-  // f*** me extension development for VSCode is buggy. I don't want to have to do this.
-  if (!pass) {
-    pass = await vscode.window.showInputBox({ prompt: "sudo password", password: true });
-  }
-  const cancel = async (pid) => {
-    let kill = await which("kill");
-    const args = [kill, "-s", "SIGUSR1", `${pid}`];
-    return await sudo(args, pass);
-  };
-  const installer = () => {
-    return which("python")
-      .then((python) => {
-        return sudo([python, repo_type], pass);
-      })
-      .then((install) => {
-        return install;
-      });
-  };
-  let listeners = { download: new EventEmitter(), install: new EventEmitter() };
+function create_ipc_server(pkgs, listeners) {
   let client_number = 0;
-  let server = net.createServer((client) => {
+  return net.createServer((client) => {
     if (client_number == 1) {
       const { packet_size, payload } = prepare_request(pkgs);
       client.write(packet_size);
@@ -78,144 +59,161 @@ async function initInstaller(repo_type, pkgs, cancellable) {
           const json = JSON.parse(message_payload);
           handle_payload(listeners, json);
         } catch (e) {
-          console.log(`Error: ${e}`);
-          vscode.window.showErrorMessage(e);
           overlap_buffer = message_payload;
         }
       }
     });
   });
-  server.listen(comms_address);
-  const unlink_unix_socket = () => {
-    console.log(`closing installer services...`);
-    try {
-      if (require("fs").existsSync(comms_address)) {
-        require("fs").unlinkSync(comms_address);
-      }
-    } catch (err) {
-      console.log(`Exception: ${err}`);
-    }
-  };
-  server.on("error", unlink_unix_socket);
-  server.on("close", unlink_unix_socket);
-  server.on("drop", unlink_unix_socket);
-
-  let remaining_download = [];
-  let installed_packages = [];
-  let processInfo = { pid: 0, ppid: 0 };
-  listeners.install.on("setup", (payload) => {
-    processInfo = payload;
-  });
-
-  listeners.download.on("setup", (payload) => {
-    processInfo = payload;
-  });
-  // eslint-disable-next-line no-unused-vars
-  listeners.download.on("start", async ({ packages, bytes }) => {
-    remaining_download = packages;
-    await vscode.window.withProgress(
-      {
-        location: vscode.ProgressLocation.Notification,
-        cancellable: cancellable,
-        title: "Downloading dependencies",
-      },
-      (reporter, token) => {
-        let p = new Promise((resolve) => {
-          const msg = (packages) => {
-            if (packages.length > 1) return `${packages.join(", ")}`;
-            else return `${packages[0] ?? "???"}`;
-          };
-
-          token.onCancellationRequested(async () => {
-            // todo(simon): implement cancelling
-            await cancel(processInfo.pid);
-          });
-
-          listeners.download.on("cancel", () => {
-            vscode.window.showInformationMessage("Download cancelled - cleaned up");
-            server.close((err) => {
-              console.log("server closed.");
-              if (err) {
-                logger.appendLine("Could not close InstallingManager connection. Remove ");
-              }
-            });
-            resolve();
-          });
-
-          listeners.download.on("done", ({ done }) => {
-            installed_packages.push(done);
-            logger.appendLine(`Downloading of ${done} - done`);
-            remaining_download = remaining_download.filter((pkg_name) => pkg_name != done);
-            reporter.report({ message: msg(remaining_download) });
-          });
-
-          // eslint-disable-next-line no-unused-vars
-          listeners.download.on("update", ({ bytes, progress, increment }) => {
-            reporter.report({
-              increment: increment,
-              message: msg(remaining_download),
-            });
-          });
-
-          listeners.download.on("finish", (payload) => {
-            vscode.window.showInformationMessage(`Downloaded ${payload.bytes}`);
-            resolve();
-          });
-        });
-        return p;
-      }
-    );
-  });
-
-  listeners.install.on("start", async () => {
-    await vscode.window.withProgress(
-      {
-        location: vscode.ProgressLocation.Notification,
-        cancellable: cancellable,
-        title: "Installing dependencies",
-      },
-      (reporter, canceller) => {
-        let p = new Promise((resolve) => {
-          canceller.onCancellationRequested(async () => {
-            // todo(simon): implement cancelling
-            await cancel(processInfo.pid);
-          });
-
-          listeners.install.on("update", (payload) => {
-            reporter.report({ message: `Installing ${payload.package}...`, increment: payload.increment });
-          });
-
-          listeners.install.on("cancel", () => {
-            vscode.window.showInformationMessage("Installation cancelled... Removed installed packages");
-            server.close((err) => {
-              if (err) {
-                logger.appendLine("Could not close InstallingManager connection. Remove ");
-              }
-            });
-            resolve();
-          });
-
-          listeners.install.on("finish", () => {
-            vscode.window.showInformationMessage(`Finished installing: ${installed_packages.join(" ")}`);
-            server.close((err) => {
-              if (err) {
-                logger.appendLine("Could not close InstallingManager connection. Remove ");
-              }
-            });
-            resolve();
-          });
-        });
-        return p;
-      }
-    );
-  });
-  await installer();
 }
 
-function handle_payload(listeners, comms_payload) {
-  listeners[comms_payload.action].emit(comms_payload.type, comms_payload.data);
+/**
+ * Run depedency/package installer
+ * @param {string} repo_type - whether we're using apt or dnf
+ * @param {string[]} pkgs - list of depedencies to install
+ * @param {boolean} cancellable - Whether or not the install operation can be cancelled
+ */
+function run_install(repo_type, pkgs, cancellable) {
+  return new Promise(async (iresolve, ireject) => {
+    let logger = vscode.window.createOutputChannel("Installing RR dependencies", "Log");
+    logger.show();
+    // eslint-disable-next-line max-len
+    let pass = await vscode.window.showInputBox({ prompt: "sudo password", password: true });
+    // f*** me extension development for VSCode is buggy. I don't want to have to do this.
+    if (!pass) {
+      pass = await vscode.window.showInputBox({ prompt: "sudo password", password: true });
+    }
+    const cancel = async (pid) => {
+      let kill = await which("kill");
+      const args = [kill, "-s", "SIGUSR1", `${pid}`];
+      await sudo(args, pass);
+    };
+    const installer = () => {
+      return which("python")
+        .then((python) => {
+          return sudo([python, repo_type], pass);
+        })
+        .then((install) => {
+          return install;
+        });
+    };
+    let listeners = { download: new EventEmitter(), install: new EventEmitter() };
+    const server = create_ipc_server(pkgs, listeners);
+    server.listen(comms_address);
+    const unlink_unix_socket = () => {
+      try {
+        if (require("fs").existsSync(comms_address)) {
+          require("fs").unlinkSync(comms_address);
+        }
+      } catch (err) {
+        console.log(`Exception: ${err}`);
+      }
+    };
+    server.on("error", () => {
+      unlink_unix_socket();
+      ireject("Installer services failed");
+    });
+    server.on("close", unlink_unix_socket);
+    server.on("drop", unlink_unix_socket);
+
+    let remaining_download = [];
+    let installed_packages = [];
+    let processInfo = { pid: 0, ppid: 0 };
+    listeners.install.on("setup", (payload) => {
+      processInfo = payload;
+    });
+
+    listeners.download.on("setup", (payload) => {
+      processInfo = payload;
+    });
+    // eslint-disable-next-line no-unused-vars
+    listeners.download.on("start", async ({ packages, bytes }) => {
+      remaining_download = packages;
+      await vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          cancellable: cancellable,
+          title: "Downloading dependencies",
+        },
+        (reporter, token) => {
+          return new Promise((resolve) => {
+            listeners.download.on("finish", resolve); // resolve this progress window, but not the installation progress (iresolve)
+
+            token.onCancellationRequested(async () => {
+              await cancel(processInfo.pid);
+            });
+
+            listeners.download.on("cancel", () => {
+              server.close((err) => {
+                console.log("server closed.");
+                if (err) {
+                  logger.appendLine("Could not close InstallingManager connection.");
+                }
+              });
+              ireject("Cancelled installing");
+            });
+
+            listeners.download.on("done", ({ done }) => {
+              installed_packages.push(done);
+              logger.appendLine(`Downloading of ${done} - done`);
+              remaining_download = remaining_download.filter((pkg_name) => pkg_name != done);
+              reporter.report({ message: remaining_download.join(", ") });
+            });
+
+            // eslint-disable-next-line no-unused-vars
+            listeners.download.on("update", ({ bytes, progress, increment }) => {
+              reporter.report({
+                increment: increment,
+                message: remaining_download.join(", "),
+              });
+            });
+          });
+        }
+      );
+    });
+
+    listeners.install.on("start", async () => {
+      await vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          cancellable: cancellable,
+          title: "Installing dependencies",
+        },
+        (reporter, canceller) => {
+          return new Promise((resolve) => {
+            listeners.install.on("finish", () => {
+              server.close((err) => {
+                if (err) {
+                  logger.appendLine("Could not close InstallingManager connection. Remove ");
+                }
+              });
+              iresolve("Finished installing");
+              resolve();
+            });
+            canceller.onCancellationRequested(async () => {
+              await cancel(processInfo.pid);
+            });
+
+            listeners.install.on("cancel", () => {
+              server.close((err) => {
+                if (err) {
+                  logger.appendLine("Could not close InstallingManager connection. Remove ");
+                }
+              });
+              ireject("Cancelled installing"); // reject the installer
+              resolve(); // resolve the progress window promise
+            });
+
+            listeners.install.on("update", (payload) => {
+              reporter.report({ message: `Installing ${payload.package}...`, increment: payload.increment });
+            });
+          });
+        }
+      );
+    });
+    await installer();
+  });
 }
 
 module.exports = {
-  initInstaller,
+  run_install,
 };
