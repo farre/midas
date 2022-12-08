cbc950bb16601d07afd389aff58733a3cb194016
Simon Farre <simon.farre.cx@gmail.com>
2022-12-09 00:42:59 +0100

    Add a Cache Manager
    This is so we can set the path of the built RR amongst many other possible things in the future.
    
    When RR gets built from source, we store that path in the global permanent cache,
    and we attempt to use that binary when launching RR.

--- modules/activateDebuggerExtension.js
+++ modules/activateDebuggerExtension.js
@@ -10,7 +10,7 @@ const { CheckpointsViewProvider } = require("./ui/checkpoints/checkpoints");
  * @param {vscode.ExtensionContext} context
  * @param {vscode.DebugAdapterDescriptorFactory} [descriptorFactory]
  */
-function activateExtension(context, descriptorFactory) {
+async function activateExtension(context, descriptorFactory) {
   const cp_provider = new CheckpointsViewProvider(context);
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(cp_provider.type, cp_provider, {
@@ -41,6 +41,21 @@ function activateExtension(context, descriptorFactory) {
   context.subscriptions.push(
     vscode.debug.registerDebugAdapterDescriptorFactory(rrProvider.type, new RRDebugAdapterFactory(cp_provider))
   );
+  const CacheManager = {
+    read: () => {
+      return context.globalState.get("MidasCache", {
+        cache: context.globalState.get("MidasCache", {
+          rr: { path: "rr", version: undefined },
+          gdb: { path: "gdb", version: undefined },
+        }),
+      });
+    },
+    write: async (cache) => {
+      context.globalState.update("MidasCache", { cache });
+    },
+  };
+  let { cache } = CacheManager.read();
+  return { CacheManager };
 }
 
 function deactivateExtension() {}
