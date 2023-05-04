a283908089d7db23c8b55a82597c1ca1d03c70db
Simon Farre <simon.farre.cx@gmail.com>
2023-05-04 21:30:04 +0200

    Update attach test subject to loop forever

--- test/cppworkspace/attach/src/main.cpp
+++ test/cppworkspace/attach/src/main.cpp
@@ -11,8 +11,13 @@ int main(int argc, const char** argv) {
   std::cout << pid << std::endl;
   // let's interrupt here, so that we are given time to attach with gdb
   std::string buf;
-  std::getline(std::cin, buf);
-  std::cout << "read: " << buf << std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(45));
+  while(true) {
+    std::cout << "input: (waiting for q to quit):";
+    std::getline(std::cin, buf);
+    std::cout << "read: " << buf << std::endl;
+    if(buf == "q") {
+      break;
+    }
+  }
   std::cout << "swoosh" << std::endl;
 }
