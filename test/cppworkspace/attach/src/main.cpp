efef23ad657bdeeeec43853170792b30f11b2ed0
Simon Farre <simon.farre.cx@gmail.com>
2023-05-04 19:51:41 +0200

    Test subject for attach functionality updated
    Reads a line from input, where we can attach gdb
    
    Signed-off-by: Simon Farre <simon.farre.cx@gmail.com>

--- test/cppworkspace/attach/src/main.cpp
+++ test/cppworkspace/attach/src/main.cpp
@@ -10,6 +10,9 @@ int main(int argc, const char** argv) {
   auto pid = getpid();
   std::cout << pid << std::endl;
   // let's interrupt here, so that we are given time to attach with gdb
+  std::string buf;
+  std::getline(std::cin, buf);
+  std::cout << "read: " << buf << std::endl;
   std::this_thread::sleep_for(std::chrono::seconds(45));
   std::cout << "swoosh" << std::endl;
 }
